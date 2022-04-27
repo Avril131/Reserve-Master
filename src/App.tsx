cf05574ddc7eed541ab07537f5aacad115faf62e
Avril131 <98386418+Avril131@users.noreply.github.com>
2022-04-27 14:37:44 +0800

    Init the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import React from 'react';
+import 'antd/dist/antd.css';
+import './App.less';
+const { Header, Content, Footer, Sider } = Layout;
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
